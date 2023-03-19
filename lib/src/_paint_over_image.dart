4f25f59e1b5bce93404e658526f61bf308322f67
nathCode45 <42984679+nathCode45@users.noreply.github.com>
2023-03-19 14:02:04 -0400

    unexpanded build controls

--- lib/src/_paint_over_image.dart
+++ lib/src/_paint_over_image.dart
@@ -498,13 +498,16 @@ class ImagePainterState extends State<ImagePainter> {
                   scaleEnabled: widget.isScalable!,
                   onInteractionUpdate: _scaleUpdateGesture,
                   onInteractionEnd: _scaleEndGesture,
-                  child: CustomPaint(
-                    size: imageSize,
-                    willChange: true,
-                    isComplex: true,
-                    painter: DrawImage(
-                      image: _image,
-                      controller: _controller,
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    child: CustomPaint(
+                      size: imageSize,
+                      willChange: true,
+                      isComplex: true,
+                      painter: DrawImage(
+                        image: _image,
+                        controller: _controller,
+                      ),
                     ),
                   ),
                 );

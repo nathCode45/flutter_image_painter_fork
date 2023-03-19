1289995f0b993c5243a79ee30686afe2c89fbd88
nathCode45 <42984679+nathCode45@users.noreply.github.com>
2023-03-19 12:33:47 -0400

    unexpanded build controls

--- lib/src/_paint_over_image.dart
+++ lib/src/_paint_over_image.dart
@@ -478,44 +478,47 @@ class ImagePainterState extends State<ImagePainter> {
     return Container(
       height: widget.height ?? double.maxFinite,
       width: double.maxFinite,//widget.width ?? double.maxFinite,
-      child: Column(
-        //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          if (widget.controlsAtTop) _buildControls(),
-          Expanded(
-            child: FittedBox(
-              fit: BoxFit.fill,
-              alignment: FractionalOffset.center,
-              child: ClipRect(
-                child: AnimatedBuilder(
-                  animation: _controller,
-                  builder: (context, child) {
-                    return InteractiveViewer(
-                      transformationController: _transformationController,
-                      maxScale: 2.4,
-                      minScale: 1,
-                      panEnabled: _controller.mode == PaintMode.none,
-                      scaleEnabled: widget.isScalable!,
-                      onInteractionUpdate: _scaleUpdateGesture,
-                      onInteractionEnd: _scaleEndGesture,
-                      child: CustomPaint(
-                        size: imageSize,
-                        willChange: true,
-                        isComplex: true,
-                        painter: DrawImage(
-                          image: _image,
-                          controller: _controller,
+      child: FittedBox(
+        fit:BoxFit.fill,
+        child: Column(
+          //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            if (widget.controlsAtTop) _buildControls(),
+            Expanded(
+              child: FittedBox(
+                fit: BoxFit.fill,
+                alignment: FractionalOffset.center,
+                child: ClipRect(
+                  child: AnimatedBuilder(
+                    animation: _controller,
+                    builder: (context, child) {
+                      return InteractiveViewer(
+                        transformationController: _transformationController,
+                        maxScale: 2.4,
+                        minScale: 1,
+                        panEnabled: _controller.mode == PaintMode.none,
+                        scaleEnabled: widget.isScalable!,
+                        onInteractionUpdate: _scaleUpdateGesture,
+                        onInteractionEnd: _scaleEndGesture,
+                        child: CustomPaint(
+                          size: imageSize,
+                          willChange: true,
+                          isComplex: true,
+                          painter: DrawImage(
+                            image: _image,
+                            controller: _controller,
+                          ),
                         ),
-                      ),
-                    );
-                  },
+                      );
+                    },
+                  ),
                 ),
               ),
             ),
-          ),
-          if (!widget.controlsAtTop) _buildControls(),
-          //SizedBox(height: MediaQuery.of(context).padding.bottom)
-        ],
+            if (!widget.controlsAtTop) _buildControls(),
+            //SizedBox(height: MediaQuery.of(context).padding.bottom)
+          ],
+        ),
       ),
     );
   }

222a3dc2a4e4622290e11e8c7dcc8b731408c534
nathCode45 <42984679+nathCode45@users.noreply.github.com>
2023-03-19 13:37:58 -0400

    unexpanded build controls

--- lib/src/_paint_over_image.dart
+++ lib/src/_paint_over_image.dart
@@ -494,7 +494,7 @@ class ImagePainterState extends State<ImagePainter> {
                     builder: (context, child) {
                       return InteractiveViewer(
                         transformationController: _transformationController,
-                        maxScale: 2.4,
+                        //maxScale: 2.4,
                         minScale: 1,
                         panEnabled: _controller.mode == PaintMode.none,
                         scaleEnabled: widget.isScalable!,

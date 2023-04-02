b30951aeb4083108a9500c8d44b67ee8dd9fbdec
nathCode45 <42984679+nathCode45@users.noreply.github.com>
2023-04-02 12:55:02 -0400

    trying to fix offset bug

--- lib/src/_paint_over_image.dart
+++ lib/src/_paint_over_image.dart
@@ -478,40 +478,45 @@ class ImagePainterState extends State<ImagePainter> {
     return Container(
       color: Colors.black,
       //decoration: Paint(),
-      // height: widget.height ?? double.maxFinite,
-      // width: widget.width ?? double.maxFinite,
+      height: widget.height ?? double.maxFinite,
+      width: widget.width ?? double.maxFinite,
       child: Column(
         //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
           if (widget.controlsAtTop) _buildControls(),
-          AnimatedBuilder(
-            animation: _controller,
-            builder: (context, child) {
-              return InteractiveViewer(
-                transformationController: _transformationController,
-                maxScale: 2.4,
-                minScale: 1,
-                panEnabled: _controller.mode == PaintMode.none,
-                scaleEnabled: widget.isScalable!,
-                onInteractionUpdate: _scaleUpdateGesture,
-                onInteractionEnd: _scaleEndGesture,
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  child: CustomPaint(
-                    size: imageSize,
-                    willChange: true,
-                    isComplex: true,
-                    painter: DrawImage(
-                      image: _image,
-                      controller: _controller,
+          FittedBox(
+            alignment: FractionalOffset.center,
+            child: ClipRect(
+              child: AnimatedBuilder(
+                animation: _controller,
+                builder: (context, child) {
+                  return InteractiveViewer(
+                    transformationController: _transformationController,
+                    maxScale: 2.4,
+                    minScale: 1,
+                    panEnabled: _controller.mode == PaintMode.none,
+                    scaleEnabled: widget.isScalable!,
+                    onInteractionUpdate: _scaleUpdateGesture,
+                    onInteractionEnd: _scaleEndGesture,
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width,
+                      child: CustomPaint(
+                        size: imageSize,
+                        willChange: true,
+                        isComplex: true,
+                        painter: DrawImage(
+                          image: _image,
+                          controller: _controller,
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-              );
-            },
+                  );
+                },
+              ),
+            ),
           ),
           if (!widget.controlsAtTop) _buildControls(),
-          //SizedBox(height: MediaQuery.of(context).padding.bottom)
+          SizedBox(height: MediaQuery.of(context).padding.bottom)
         ],
       ),
     );

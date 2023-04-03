93e42abbf642a2976fadef66bf10fddca9be7cd2
nathCode45 <42984679+nathCode45@users.noreply.github.com>
2023-04-02 22:21:52 -0400

    trying to fix offset bug

--- lib/src/_paint_over_image.dart
+++ lib/src/_paint_over_image.dart
@@ -487,35 +487,33 @@ class ImagePainterState extends State<ImagePainter> {
           Expanded(
             child: FittedBox(
               alignment: FractionalOffset.center,
-              child: ClipRect(
-                child: AnimatedBuilder(
-                  animation: _controller,
-                  builder: (context, child) {
-                    return InteractiveViewer(
-                      transformationController: _transformationController,
-                      maxScale: 2.4,
-                      minScale: 1,
-                      panEnabled: _controller.mode == PaintMode.none,
-                      scaleEnabled: widget.isScalable!,
-                      onInteractionUpdate: _scaleUpdateGesture,
-                      onInteractionEnd: _scaleEndGesture,
-                      child: CustomPaint(
-                        size: imageSize,
-                        willChange: true,
-                        isComplex: true,
-                        painter: DrawImage(
-                          image: _image,
-                          controller: _controller,
-                        ),
+              child: AnimatedBuilder(
+                animation: _controller,
+                builder: (context, child) {
+                  return InteractiveViewer(
+                    transformationController: _transformationController,
+                    maxScale: 3.4,
+                    minScale: 1,
+                    panEnabled: _controller.mode == PaintMode.none,
+                    scaleEnabled: widget.isScalable!,
+                    onInteractionUpdate: _scaleUpdateGesture,
+                    onInteractionEnd: _scaleEndGesture,
+                    child: CustomPaint(
+                      size: imageSize,
+                      willChange: true,
+                      isComplex: true,
+                      painter: DrawImage(
+                        image: _image,
+                        controller: _controller,
                       ),
-                    );
-                  },
-                ),
+                    ),
+                  );
+                },
               ),
             ),
           ),
           if (!widget.controlsAtTop) _buildControls(),
-          SizedBox(height: MediaQuery.of(context).padding.bottom)
+          //SizedBox(height: MediaQuery.of(context).padding.bottom)
         ],
       ),
     );
